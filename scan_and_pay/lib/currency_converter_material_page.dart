6538ec4a5861c5d5a126e11e38f52f8e055253b9
Maanvi Shadakshari <maanvishadakshari21@gmail.com>
2024-06-04 13:03:46 +0530

    update #2

--- scan_and_pay/lib/currency_converter_material_page.dart
+++ scan_and_pay/lib/currency_converter_material_page.dart
@@ -4,9 +4,30 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Text("Currency Page!"),
-    ));
+    return const Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '0',
+            style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255)),
+          ),
+          TextField(
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              hintText: "Please enetr the amount in USD",
+              hintStyle: TextStyle(color: Colors.white60),
+              prefixIcon: Icon(Icons.monetization_on_outlined),
+              prefixIconColor: Colors.white,
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
